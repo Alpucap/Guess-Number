43019b63542bb0a9c0a43de070283da37725b3ea
unknown <alpucaps@gmail.com>
2024-11-06 10:02:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('Please click one of the number buttons on the screen...');
+  const [ptext, setpText] = useState();
+
+  const maxNum = 5;
+  const randomNumber = () => {
+    let ChoosenOne = Math.floor(Math.random() * maxNum) + 1;
+
+    const handleButton = (buttonNumber) => {
+      if(buttonNumber === ChoosenOne){
+        setText("You Win!");
+        setpText("Please Wait...")
+        setTimeout (() => {
+          handleRefresh();
+        }, 2000)
+      }
+      else{
+        setText(`You Lose! The right number is ${ChoosenOne}`);
+        setpText("Please Wait...")
+        setTimeout (() => {
+          handleRefresh();
+        }, 2000)
+      }
+    };
+    return handleButton;
+  }
+  function handleRefresh() {
+    window.location.reload(); 
+  }
+
+  const handleClick = randomNumber();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <h1>{text}</h1>
+        <div className="Buttons">
+          <button className="1" onClick={() => handleClick(1)}>1</button>
+          <button className="2" onClick={() => handleClick(2)}>2</button>
+          <button className="3" onClick={() => handleClick(3)}>3</button>
+          <button className="4" onClick={() => handleClick(4)}>4</button>
+          <button className="5" onClick={() => handleClick(5)}>5</button>
+        </div>
+        <p>{ptext}</p>
+      </div>
+      <footer>Made by Hans Christian Handoto</footer>
     </div>
   );
 }
